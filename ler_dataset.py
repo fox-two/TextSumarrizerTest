6140555059d52e4570cb4b7da6178212c433dcd7
namae <testeub12737@8237.com>
2018-11-08 18:19:06 -0300

    no comment

--- ler_dataset.py
+++ ler_dataset.py
@@ -20,44 +20,20 @@ def getTrainingData():
 	dataset = []
 	texto_original = []
 
-	if os.path.isfile('emails_processados.json'):
-		with open('emails_processados.json', 'r') as f:
-			dados = json.load(f)
-			return dados[0], dados[1]
 	
 	print('Lendo emails...')
-	with open('dados_email.csv', encoding='utf-8') as csvfile:
+	with open('articles.csv', encoding='utf-8') as csvfile:
 		table = csv.reader(csvfile, delimiter=',', quotechar='"')
 		for i, row in enumerate(table):
 			if i == 0:
 				continue		
 			print("Email %d" % i)
-			frase_original = row[4].lower()
-			frase_processada = processa_frase(frase_original)
-			texto_original.append(frase_original)
+			frase_original = row[1].lower()
+			frase_processada = processa_frase(frase_original, False)
+
+			#texto_original.append(frase_original)
 			dataset.append(frase_processada)
 
 	print("%i emails lidos" % len(dataset))
 
-	with open('emails_processados.json', 'w') as f:
-		salvar = [dataset, texto_original]
-		json.dump(salvar, f)
 	return dataset, texto_original
-
-def getPhrases():
-	if os.path.isfile('frases.json'):
-		with open('frases.json', 'r') as f:
-			dados = json.load(f)
-			return dados
-
-	frases = []
-	dados, texto_original = getTrainingData()
-	
-	for item in texto_original:
-		linhas = item.split('\n')
-		for linha in linhas:
-			linhas_separadas = [processa_frase(x, False) for x in sent_tokenize(linha)]
-			frases.extend([x for x in linhas_separadas if len(x) > 0])
-	with open('frases.json', 'w') as f:
-		json.dump(frases, f)
-	return frases			
--- word2vec.py
+++ word2vec.py
@@ -13,70 +13,17 @@ import gensim
 from gensim.utils import simple_preprocess
 import spacy
 from nltk.tokenize import sent_tokenize
+from ler_dataset import getTrainingData
 
-
-stemmer = SnowballStemmer("portuguese")
-nlp = None#spacy.load('pt', disable=['parser', 'tagger', 'ner', 'textcat', 'tokenizer'])
-
-def processa_frase(frase):
-	palavras = gensim.utils.simple_preprocess(str(frase), deacc=True)
-	return [x for x in palavras]
-
-def getTrainingData():
-		dataset = []
-		texto_original = []
-
-		if os.path.isfile('emails_processados.json'):
-				with open('emails_processados.json', 'r') as f:
-						dados = json.load(f)
-						return dados[0], dados[1]
-
-		
-		print('Lendo emails...')
-		with open('dados_email.csv', encoding='utf-8') as csvfile:
-				table = csv.reader(csvfile, delimiter=',', quotechar='"')
-				for i, row in enumerate(table):
-						if i == 0:
-								continue
-						print("Email %d" % i)
-						
-						linhas = row[4].lower()
-						frase_atual = processa_frase(linhas)
-
-						dataset.append(frase_atual)
-						texto_original.append(linhas)
-
-
-		print("%i emails lidos" % len(dataset))
-
-		with open('emails_processados.json', 'w') as f:
-				salvar = [dataset, texto_original]
-				json.dump(salvar, f)
-		return dataset, texto_original
+import logging
+import sys
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 dados, texto_original = getTrainingData()
 
-model = Word2Vec(dados, size=35, window=3, min_count=10, workers=4, sg=1, iter=300)
+model = Word2Vec(dados, size=150, window=3, min_count=10, workers=4, sg=1, iter=10)
 model = model.wv
 model.save("modelo_word2vec")
 
-
-
-vetor_pesquisa = model['motorista']
-
-from sklearn.metrics.pairwise import cosine_similarity
-
-distancias = [(model.index2word[i], resultado[0]) for i, resultado in enumerate(cosine_similarity(model.vectors, [vetor_pesquisa]))]
-
-distancias = sorted(distancias, key=lambda x: x[1], reverse=True)
-
-from pprint import pprint
-
-
-pprint(distancias[0:20])
-
-print("fim")
-#from gensim.models import KeyedVectors
-#model = KeyedVectors.load_word2vec_format('glove_s50.txt')
 
 
