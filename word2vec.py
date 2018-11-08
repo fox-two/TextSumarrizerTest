e548d87cdcfc0baf4b27d263bc6b8fc18704e1ab
namae <testeub12737@8237.com>
2018-11-08 18:25:00 -0300

    no comment

--- word2vec.py
+++ word2vec.py
@@ -21,7 +21,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 dados, texto_original = getTrainingData()
 
-model = Word2Vec(dados, size=150, window=3, min_count=10, workers=4, sg=1, iter=10)
+model = Word2Vec(dados, size=150, window=10, min_count=10, workers=8, sg=1, iter=30)
 model = model.wv
 model.save("modelo_word2vec")
 
